83d011aa88d71443431c934c385f916fe18322d1
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-28 12:43:14 +0530

    adding error messages

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -9,6 +9,26 @@ module.exports = (err,req,res,next) => {
         err = new ErrorHandler(message,400)
     }
 
+    // Mongoose duplicate key error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message,400)
+    }
+
+    // Wrong JWT error
+    if(err.name==="JsonWebTokenError"){
+        const message = `json web token is invalid try Again `;
+
+        err = new ErrorHandler(message,400)
+    }
+
+    // Jwt expire
+    if(err.name==="JsonWebTokenError"){
+        const message = `json web token is expired try Again `;
+
+        err = new ErrorHandler(message,400)
+    }
+
     res.status(err.statusCode).json({
         success :false,
         message :err.message
