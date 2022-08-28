7176dd3af7145949893e3fbc2d28a2fd6dd02ecb
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-28 12:43:54 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const { registerUser, loginUser, logoutUser, forgetPassword, resetPassword } = require("../controllers/userController");
+const { registerUser, loginUser, logoutUser, forgetPassword, resetPassword,
+     getUserDetails, updatePassword, updateProfile, getAllUsers, getSingleUser } = require("../controllers/userController");
+const { isAuthenticatedUser , authorizeRole} = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/registerUser").post(registerUser);
@@ -7,4 +9,9 @@ router.route("/login").post(loginUser)
 router.route("/logout").get(logoutUser)
 router.route("/password/forget").post(forgetPassword)
 router.route("/password/reset/:token").put(resetPassword)
+router.route("/me").get(isAuthenticatedUser,getUserDetails)
+router.route("/password/update").put( isAuthenticatedUser,updatePassword)
+router.route("/me/update").put( isAuthenticatedUser,updateProfile)
+router.route("/admin/users").get(isAuthenticatedUser,authorizeRole("admin"), getAllUsers)
+router.route("/admin/user/:id").get(isAuthenticatedUser,authorizeRole("admin"), getSingleUser)
 module.exports = router;
