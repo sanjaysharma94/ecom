4684d1303f23799e42a98742a04619936866acf4
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-29 09:28:54 +0530

    updating user routes for profile update  by admin

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { registerUser, loginUser, logoutUser, forgetPassword, resetPassword,
-     getUserDetails, updatePassword, updateProfile, getAllUsers, getSingleUser } = require("../controllers/userController");
+     getUserDetails, updatePassword, updateProfile, getAllUsers, getSingleUser ,updateUserRole } = require("../controllers/userController");
 const { isAuthenticatedUser , authorizeRole} = require("../middleware/auth");
 const router = express.Router();
 
@@ -13,5 +13,9 @@ router.route("/me").get(isAuthenticatedUser,getUserDetails)
 router.route("/password/update").put( isAuthenticatedUser,updatePassword)
 router.route("/me/update").put( isAuthenticatedUser,updateProfile)
 router.route("/admin/users").get(isAuthenticatedUser,authorizeRole("admin"), getAllUsers)
-router.route("/admin/user/:id").get(isAuthenticatedUser,authorizeRole("admin"), getSingleUser)
+router.route("/admin/user/:id")
+.get(isAuthenticatedUser,authorizeRole("admin"), getSingleUser)
+.put(isAuthenticatedUser,authorizeRole("admin"),updateUserRole)
+
+
 module.exports = router;
