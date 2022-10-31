3136e49eccdda139bf55ba3eef5760e8f1284fda
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-10-31 12:38:59 +0530

    creating api for getting all reviews and deleting a review

--- backend/controllers/productcontroller.js
+++ backend/controllers/productcontroller.js
@@ -143,11 +143,38 @@ exports.getProductReviews = catchAsyncErrors(async(req,res,next) =>{
 
 exports.deleteReview = catchAsyncErrors(async(req,res,next) =>{
 
-    const product = await Product.findById(req.query.id);
+    const product = await Product.findById(req.query.productId);
 
     if(!product){
         return next(new ErrorHandler("product not found", 404));
     }
+
+    const reviews = product.reviews.filter(
+        (rev)=> rev._id.toString()!== req.query.id.toString());
+
+        let avg = 0;
+         reviews.forEach(rev=>{
+            avg += rev.rating
+        });
+
+        const ratings = avg/reviews.length;
+        
+        const numOfReviews = reviews.length;
+
+        await Product.findByIdAndUpdate(req.query.productId,{
+            reviews,
+            ratings,
+            numOfReviews
+        },{
+            new : true,
+            runValidators:true,
+            useFindAndModify:false,
+        });
+
+        res.status(200).json({
+            success:true
+        })
+        
 
 });
 
