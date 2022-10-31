9758e34fadd0fb027ae074237dc2425e33788b89
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-10-31 22:51:10 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,11 @@ app.use(express.json())
 app.use(cookieParser())
 const products = require("./routes/productRoute")
 const user = require("./routes/userRoute")
+const order = require("./routes/orderRoute")
 
 app.use("/api/v1",products)
 app.use("/api/v1",user)
+app.use("/api/v1",order)
 
 
 app.use(errorMiddleware)
