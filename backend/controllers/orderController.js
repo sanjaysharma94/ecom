1b249349c1d9745e40a04906a41de595e9408a7c
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-11-01 17:34:49 +0530

    added api getSingleOrder and  my orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,10 +3,10 @@ const Product = require("../models/productmodels");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsynErrors");
 const ApiFeatures = require("../utils/apiFeatures");
+const userModels = require("../models/userModels");
 
 
 //create  New   order
-
 
 exports.newOrder = catchAsyncErrors(async(req,res,next)=>{
     const { shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body
@@ -28,3 +28,42 @@ exports.newOrder = catchAsyncErrors(async(req,res,next)=>{
             order
         })
 })
+
+// get single order 
+
+exports.getSingleOrder = catchAsyncErrors(async(req,res,next) =>{
+    const order = await Order.findById(req.params.id).populate({
+        path: 'user',
+        select: 'name email',
+        model: userModels
+});
+
+    if(!order){
+
+        return next(new ErrorHandler("order not found", 404));
+ 
+     }
+    res.status(200).json({
+        success:true,
+        order
+        
+    })
+});
+
+// get my order  when logged in
+
+exports.myOrders = catchAsyncErrors(async(req,res,next) =>{
+    const orders = await Order.find({user : req.user._id});
+
+    if(!orders){
+
+        return next(new ErrorHandler("No previous orders found", 404));
+ 
+     }
+    res.status(200).json({
+        success:true,
+        orders,
+        "totalorders":orders.length
+        
+    })
+});
