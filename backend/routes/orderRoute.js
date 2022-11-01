2cebb997df73e7e091d8f446d746224bdd02e4fc
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-11-01 17:32:16 +0530

    added new routes for my orders and order(admin)

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { newOrder } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRole } = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/order/new").post( isAuthenticatedUser,newOrder)
+router.route("/order/new").post(isAuthenticatedUser,newOrder)
+router.route("/order/:id").get( isAuthenticatedUser, authorizeRole("admin"), getSingleOrder)
+router.route("/orders/me").get( isAuthenticatedUser, myOrders)
+
 
 
 module.exports = router
