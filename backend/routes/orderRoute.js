8b9c3ae0a519496a5373b8707f5e5f3ce0e7aba9
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-11-04 16:51:15 +0530

    added admin order routes

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRole } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/order/new").post(isAuthenticatedUser,newOrder)
 router.route("/order/:id").get( isAuthenticatedUser, getSingleOrder)
 router.route("/orders/me").get( isAuthenticatedUser, myOrders)
+
+router.route("/admin/orders").get(isAuthenticatedUser, authorizeRole("admin"), getAllOrders);
+router.route("/admin/order/:id").put(isAuthenticatedUser, authorizeRole("admin"), updateOrder);
+router.route("/admin/order/:id").delete(isAuthenticatedUser, authorizeRole("admin"), deleteOrder);
+
+
+
 
 
 
